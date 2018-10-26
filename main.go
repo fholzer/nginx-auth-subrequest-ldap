fef2536828ab289c26aa3e76d0b27c2a56c9d053
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2018-10-26 22:47:34 +0200

    Improve logging, resolves #4

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"fmt"
 	"github.com/mavricknz/ldap"
 	"github.com/patrickmn/go-cache"
+	log "github.com/sirupsen/logrus"
 	"gopkg.in/ini.v1"
-	"log"
 	"net"
 	"net/http"
 	"net/http/fcgi"
@@ -60,11 +60,43 @@ func init() {
 	bind_pass = cfg.Section("").Key("ldap_password").String()
 	server_network = cfg.Section("").Key("server_network").String()
 	server_address = cfg.Section("").Key("server_address").String()
+	log_file := cfg.Section("").Key("log_file").String()
+	log_format := cfg.Section("").Key("log_format").String()
+	log_level := cfg.Section("").Key("log_level").String()
 	realm = cfg.Section("").Key("httpauth_realm").String()
 	ttl, _ := time.ParseDuration(cfg.Section("").Key("httpauth_cache_ttl").String())
 	neg_ttl, _ = time.ParseDuration(cfg.Section("").Key("httpauth_cache_negative_ttl").String())
 	cleanupInterval, _ := time.ParseDuration(cfg.Section("").Key("httpauth_cache_cleanup_intreval").String())
 	c = cache.New(ttl, cleanupInterval)
+
+	switch log_format {
+	case "text":
+		log.SetFormatter(&log.TextFormatter{})
+	case "json":
+		log.SetFormatter(&log.JSONFormatter{})
+	default:
+		log.Fatal("Unknown log format.")
+	}
+
+	switch log_file {
+	case "stdout":
+		log.SetOutput(os.Stdout)
+	case "stderr":
+		log.SetOutput(os.Stderr)
+	default:
+		file, err := os.OpenFile(log_file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err == nil {
+			log.SetOutput(file)
+		} else {
+			log.Fatal("Failed to open log file: ", err)
+		}
+	}
+
+	lvl, err := log.ParseLevel(log_level)
+	if err != nil {
+		log.Fatal("Invalid log level.")
+	}
+	log.SetLevel(lvl)
 }
 
 type Server struct{}
@@ -83,6 +115,9 @@ func (s *Server) authenticate(username, password string) (r bool, e error) {
 	l := ldap.NewLDAPSSLConnection(host, uint16(port), tlsConfig)
 	e = l.Connect()
 	if e != nil {
+		log.WithFields(log.Fields{
+			"error": e.Error(),
+		}).Error("Failed to connect to LDAP server.")
 		return
 	}
 
@@ -91,7 +126,9 @@ func (s *Server) authenticate(username, password string) (r bool, e error) {
 	// bind with authenticated user
 	e = l.Bind(bind_user, bind_pass)
 	if e != nil {
-		fmt.Println(e)
+		log.WithFields(log.Fields{
+			"error": e.Error(),
+		}).Error("Failed to bind to LDAP server.")
 		return
 	}
 
@@ -107,6 +144,9 @@ func (s *Server) authenticate(username, password string) (r bool, e error) {
 
 	search_result, err := l.Search(search_request)
 	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Warn("Failed to search LDAP server.")
 		return
 	}
 
@@ -135,31 +175,64 @@ func splitAuth(h string) (string, []byte, error) {
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add(wwwAuthenticate, fmt.Sprintf("Basic realm=\"%s\"", realm))
 
-	_, data, err := splitAuth(r.Header.Get(authorization))
+	headerValue := r.Header.Get(authorization)
+	_, data, err := splitAuth(headerValue)
 	if err != nil {
 		w.WriteHeader(401)
-		return
-	}
-
-	k := string(data)
-	if x, found := c.Get(k); found {
-		w.WriteHeader((x.(*entry)).response)
+		// TODO: should be Trace, as it may contain sensitive information
+		if log.IsLevelEnabled(log.DebugLevel) {
+			log.WithFields(log.Fields{
+				"headerValue": headerValue,
+			}).Debug("Malformed authorization header.")
+		}
 		return
 	}
 
 	i := bytes.IndexRune(data, ':')
 	if i < 0 {
 		w.WriteHeader(401)
+		// TODO: should be Trace, as it may contain sensitive information
+		if log.IsLevelEnabled(log.DebugLevel) {
+			log.WithFields(log.Fields{
+				"headerValue": headerValue,
+			}).Debug("Malformed authorization header.")
+		}
 		return
 	}
 	username, password := string(data[:i]), string(data[i+1:])
+
+	k := string(data)
+	if x, found := c.Get(k); found {
+		response := (x.(*entry)).response
+		w.WriteHeader(response)
+		if log.IsLevelEnabled(log.DebugLevel) {
+			log.WithFields(log.Fields{
+				"username": username,
+				"response": response,
+			}).Debug("Served response from cache.")
+		}
+		return
+	}
+
 	valid, err := s.authenticate(username, password)
 	if valid {
 		c.Set(k, &entry{response: 200}, cache.DefaultExpiration)
 		w.WriteHeader(200)
+		if log.IsLevelEnabled(log.DebugLevel) {
+			log.WithFields(log.Fields{
+				"username": username,
+				"response": 200,
+			}).Debug("Successful authentication.")
+		}
 	} else {
 		c.Set(k, &entry{response: 401}, neg_ttl)
 		w.WriteHeader(401)
+		if log.IsLevelEnabled(log.DebugLevel) {
+			log.WithFields(log.Fields{
+				"username": username,
+				"response": 401,
+			}).Debug("Failed authentication.")
+		}
 	}
 }
 
@@ -175,7 +248,7 @@ func main() {
 	go func() {
 		<-gracefulStop
 		shuttingDown = true
-		log.Println("Shutting down...")
+		log.Info("Shutting down...")
 		if listener != nil {
 			listener.Close()
 		}
@@ -185,16 +258,16 @@ func main() {
 
 	// create listener if needed
 	if server_network != "stdin" {
-		log.Printf("Starting listener on  %s://%s", server_network, server_address)
+		log.Info("Starting listener on  %s://%s", server_network, server_address)
 		listener, err = net.Listen(server_network, server_address)
 		if err != nil {
 			log.Fatal("net.Listen:", err)
 		}
 	} else {
-		log.Println("Using stdin as listener socket")
+		log.Info("Using stdin as listener socket")
 	}
 
-	log.Println("Starting server...")
+	log.Info("Starting server...")
 	if err = fcgi.Serve(listener, server); err != nil && shuttingDown == false {
 		log.Fatal(err)
 	}
